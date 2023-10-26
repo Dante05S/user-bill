ad5a975f8d8b15928d0088c59745a00eb4e897c9
Dante05S <Dante05S@hotmail.com>
2023-10-25 21:23:25 -0500

    first commit

---
+++ src/main/java/com/user/bill/userbill/UserBillApplication.java
@@ -0,0 +1,13 @@
+package com.user.bill.userbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user/bill/userbill/UserBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.bill.userbill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
